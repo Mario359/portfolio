d08590e8aebf3a5f4990ed65cdd5ba91f16fdcce
Luc Allardin <luc.allardin@free.fr>
2021-05-04 10:52:51 +0200

    Texte des situations professionnelles

--- lib/pages/professional_situation_1.dart
+++ lib/pages/professional_situation_1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/widget/paragraph.dart';
 
 class ProfessionalSituation1 extends StatefulWidget {
   @override
@@ -22,7 +23,26 @@ class _ProfessionalSituation1State extends State<ProfessionalSituation1> {
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(128.0, 64.0, 128.0, 128.0),
                 child: Container(
-                  child: Text("Texte"),
+                  child: Column(
+                    children: [
+                      Paragraph(
+                        text:
+                            "Contexte: SAFI est une multinationale française dont les activités incluent la pharmacie (notamment des médicaments de prescription dans les domaines du diabète, des maladies rares, de la sclérose en plaques et de l'oncologie, des produits de santé grand public et des génériques), les vaccins et la santé animale. Pour gérer l'évolution d'une partie de son Système d'Information (SI), SAFI a réalisé un appel d'offres auprès de plusieurs entreprises du secteur informatique (ESN).",
+                      ),
+                      Paragraph(
+                        text:
+                            "La société IConseil spécialisée dans l'informatique de développement basée à la Roche sur Yon a été choisie par SAFI pour l'accompagner dans sa démarche d'évolution.",
+                      ),
+                      Paragraph(
+                        text:
+                            "Mon objectif: Réaliser une application mobile pour aider le visiteur médical dans la déclaration de ses frais d'activités.",
+                      ),
+                      Paragraph(
+                        text:
+                            "L'application comporte une authentification, un accès aux fichies de frais, un rensegnement de la fiche de frais du mois en cours, une gestion du profil de l'utilisateur et un accès aux historiques des fiches.",
+                      ),
+                    ],
+                  ),
                 ),
               ),
             )
--- lib/pages/professional_situation_2.dart
+++ lib/pages/professional_situation_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/widget/paragraph.dart';
 
 class ProfessionalSituation2 extends StatefulWidget {
   @override
@@ -22,7 +23,22 @@ class _ProfessionalSituation2State extends State<ProfessionalSituation2> {
               child: Padding(
                 padding: const EdgeInsets.fromLTRB(128.0, 64.0, 128.0, 128.0),
                 child: Container(
-                  child: Text("Texte"),
+                  child: Column(
+                    children: [
+                      Paragraph(
+                        text:
+                            "Contexte: Le groupe SEVEN a été créé en 1915 en France et est spécialisé dans le service de location de véhicules exclusivement pour des clients grands comptes (donc pas pour des particuliers). Aujourd’hui, le groupe compte jusqu’à 200 agences en France. Pour améliorer son Système d'Information, elle fait appel aux compétences de l'ESN Atos reconnue leader des services IoT.",
+                      ),
+                      Paragraph(
+                        text:
+                            "Mon objectif: Réaliser une application desktop pour gérer l'activité des agences.",
+                      ),
+                      Paragraph(
+                        text:
+                            "L'application comporte une authentification, les caractéristiques des réservations, une gestion de parcs automobiles, les statistiques sur les réservations, et une gestion du profil",
+                      )
+                    ],
+                  ),
                 ),
               ),
             )
